c5e82bccbb0160a4bd27480bb6647d8eddb8c0d8
Creslyn Gayle <146447175+LovelyJavier@users.noreply.github.com>
2026-08-03 11:45:36 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -251,39 +251,3 @@ filterButtons.forEach(button => {
     });
 
 });
-// ==========================================
-    // Dark Mode
-    // ==========================================
-
-    const themeButton = document.getElementById("theme-toggle");
-
-    if(localStorage.getItem("theme") === "dark"){
-
-        document.body.classList.add("dark-mode");
-
-        themeButton.textContent = "☀️";
-
-    }
-
-    themeButton.addEventListener("click", () => {
-
-        document.body.classList.toggle("dark-mode");
-
-        if(document.body.classList.contains("dark-mode")){
-
-            themeButton.textContent = "☀️";
-
-            localStorage.setItem("theme","dark");
-
-        }else{
-
-            themeButton.textContent = "🌙";
-
-            localStorage.setItem("theme","light");
-
-        }
-
-    });
-
-});
-
